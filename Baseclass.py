4cb2b5a0d3c7cc5a727d3e5c31d72ac3736bef83
Adib-m07 <Adib.Mozumder@laetottenham.ac.uk>
2024-11-28 10:03:51 +0000

    added stuff

---
+++ Baseclass.py
@@ -0,0 +1,30 @@
+class Pupil:
+    def__init__(name,age,given_gender):
+        self.name=name
+        self.age=age
+        self.given_gender=given_gender
+    
+    def set_details(self):
+        name=self.name
+        age=self.age
+        given_age=self.given_age
+    def get_details(self);
+        print(f{'name',name /n 'age', age/n 'given_gender', given_gender/name})
+
+class Student(Pupil):
+    def_init_(name,age,given_gender,student_id,course,grades):
+        self.student_id=student_id
+        self.course=course
+        self.grades
+
+    def set_student_details(self.student_id,self.course):
+        student_id=self.student_id
+        course=self.course
+        print(f{'student_id',student_id /n 'course', course/n })
+    
+    def add_grade(grade):
+        self.grade.append(self.grade)
+    
+    def calculate_average_grade():
+        
+
